806eec0a1f4cbd27726b6cde81f27be6d1f4d0a4
AJ The Developer <ajthedeveloper@gmail.com>
2021-11-18 18:12:23 +0200

    Updated controller

--- api/models/sprintSchema.js
+++ api/models/sprintSchema.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 
 const sprintSchema =  mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    sprName: {type: String, required: false},
+    sprNumber: {type: Number, required: false},
     project: {type: String, ref: 'Projects'},
+    startDate: {type: Date},
+    endDate: {type: Date},
+    sprintType: {type:number},
     lists: [
         {
             _id: mongoose.Schema.Types.ObjectId,
