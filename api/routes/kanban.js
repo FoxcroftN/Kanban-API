b118585e3370fff0c730dc24b7fe57e225ccabb1
AJ The Developer <ajthedeveloper@gmail.com>
2021-11-18 18:02:30 +0200

    Edited URL

--- api/routes/kanban.js
+++ api/routes/kanban.js
@@ -4,11 +4,10 @@ const router = express.Router();
 const KanbanController = require('../controllers/kanban') ;
 require('dotenv').config();
 
-router.get('/',KanbanController.get_lists);
 
-router.get('/:sprintId', KanbanController.get_lists);
+router.get('/', KanbanController.get_lists);
 
-router.patch('/:sprintId', KanbanController.update_lists);
+router.patch('/', KanbanController.update_lists);
 
 
 module.exports = router;
